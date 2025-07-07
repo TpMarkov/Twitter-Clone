e43c288474ef0ac91f957cba875b39419018a23c
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-07 09:03:54 +0300

    add some small change

--- backend/src/controllers/notification.controller.js
+++ backend/src/controllers/notification.controller.js
@@ -12,7 +12,7 @@ export const getNotifications = asyncHandler(async (req, res) => {
   const notifications = await Notification.find({
     to: user._id,
   })
-    .sort({ addedAt: -1 })
+    .sort({ createdAt: -1 })
     .populate("from", "username firstName lastName profilePicture")
     .populate("post", "content image")
     .populate("comment", "content");
