4711f60330cd347a14c80013c8a8911ebb4716c7
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-09 09:11:53 +0300

    add some logout button on home page

--- mobile/Twitter-Clone/app/index.tsx
+++ mobile/Twitter-Clone/app/index.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
+import { useClerk } from "@clerk/clerk-expo";
 
 const index = () => {
+  const { signOut } = useClerk();
+
   return (
     <View>
       <Text>Home Screen</Text>
+      <Button onPress={() => signOut()} title="logout"></Button>
     </View>
   );
 };
