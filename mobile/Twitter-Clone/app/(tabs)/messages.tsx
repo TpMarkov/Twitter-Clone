28a810e65c1b5338d3912138ae710c7aa3ecd3da
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-09 15:47:13 +0300

    add some render messages with hardcoded data

--- mobile/Twitter-Clone/app/(tabs)/messages.tsx
+++ mobile/Twitter-Clone/app/(tabs)/messages.tsx
@@ -1,4 +1,11 @@
-import { View, Text, Alert, TouchableOpacity, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  Alert,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React from "react";
 import {
   SafeAreaView,
@@ -7,6 +14,7 @@ import {
 import { useState } from "react";
 import { CONVERSATIONS, ConversationType } from "../data/conversations";
 import { Feather } from "@expo/vector-icons";
+import { Image } from "react-native";
 
 const MessagesScreen = () => {
   const insets = useSafeAreaInsets();
@@ -68,20 +76,80 @@ const MessagesScreen = () => {
   };
 
   return (
-    <SafeAreaView className="flex-1 bg-white">
+    <SafeAreaView className="flex-1 bg-white" edges={["top"]}>
       {/* HEADER */}
-      <View className="flex-row items-center justify-between px-4 py-3 border-b border-gray-100">
-        <Text className="text-xl font-bold text-gray-900">Message</Text>
+
+      <View className="flex-row items-center justify-between border-b border-gray-100 py-4 px-3">
+        <Text className="font-bold text-xl text-gray-900">Messages</Text>
         <TouchableOpacity>
-          <Feather name="edit" size={24} color="#1DA1F2" />
+          <Feather name="edit" size={24} color="#1DA1F2"></Feather>
+        </TouchableOpacity>
+      </View>
+
+      <View className="px-4 py-3 border-b border-gray-100">
+        <View className="flex-row items-center bg-gray-100 rounded-full px-4 py-2">
+          <Feather name="search" size={20} color="#657786" />
           <TextInput
-            placeholder="Search for people and groups"
             className="flex-1 ml-3 text-base"
+            placeholder="Search for people and groups"
             placeholderTextColor="#657786"
             value={searchText}
             onChangeText={setSearchText}
-          />
-        </TouchableOpacity>
+          ></TextInput>
+        </View>
+      </View>
+
+      <ScrollView
+        className="flex-1"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 + insets.bottom }}
+      >
+        {convesationsList.map((conversation, index) => (
+          <TouchableOpacity
+            key={index}
+            className="flex-row items-center p-4 border-b border-gray-50 active:bg-gray-50"
+            onPress={() => openConversation(conversation)}
+            onLongPress={() => deleteConversation(conversation.id)}
+          >
+            <Image
+              className="size-12 rounded-full mr-3"
+              source={{ uri: conversation.user.avatar }}
+            ></Image>
+
+            <View className="flex-1">
+              <View className="flex-row items-center justify-between mb-1">
+                <View className="flex-1 flex-row gap-2 items-center">
+                  <Text className="font-semibold text-gray-900">
+                    {conversation.user.name}
+                  </Text>
+                  {conversation.user.verified && (
+                    <Feather
+                      name="check-circle"
+                      size={16}
+                      color="#1DA1F2"
+                      className="ml-1"
+                    />
+                  )}
+                  <Text className="text-gray-500 text-sm ml-1">
+                    {"@" + conversation.user.username}
+                  </Text>
+                </View>
+                <Text className="text-gray-500 text-sm">
+                  {conversation.time}
+                </Text>
+              </View>
+              <Text className="text-sm text-gray-500" numberOfLines={1}>
+                {conversation.lastMessage}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+      {/* Quick actions */}
+      <View className="px-4 py-2 border-t border-gray-50 bg-gray-50">
+        <Text className="font-sm text-gray-500 text-center">
+          Tap to open Long press to delete
+        </Text>
       </View>
     </SafeAreaView>
   );
