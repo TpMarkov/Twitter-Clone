8790132bc8fb5ef5937a0f3c123dd41a23e8c051
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-09 12:55:51 +0300

    align the button for the signOut function

--- mobile/Twitter-Clone/app/(tabs)/index.tsx
+++ mobile/Twitter-Clone/app/(tabs)/index.tsx
@@ -5,7 +5,9 @@ import SignoutButton from "@/components/SignoutButton";
 const HomeScreen = () => {
   return (
     <SafeAreaView className="flex-1">
-      <SignoutButton></SignoutButton>
+      <View className="flex-1 items-end">
+        <SignoutButton></SignoutButton>
+      </View>
     </SafeAreaView>
   );
 };
--- mobile/Twitter-Clone/components/SignoutButton.tsx
+++ mobile/Twitter-Clone/components/SignoutButton.tsx
@@ -6,7 +6,7 @@ import { useSignOut } from "@/app/hooks/useSIgnOut";
 const SignoutButton = () => {
   const { handleSignOut } = useSignOut();
   return (
-    <TouchableOpacity onPress={handleSignOut}>
+    <TouchableOpacity onPress={handleSignOut} className="py-5 px-5">
       <Feather name="log-out" size={24} color={"#E0245E"} />
     </TouchableOpacity>
   );
